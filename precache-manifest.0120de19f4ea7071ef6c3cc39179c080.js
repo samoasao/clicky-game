7bcb7c454532ac9f5155214645d0b67f899bca54
Tyler Wright <twright@theabr.org>
2019-06-11 15:31:18 -0700

    Updates

---
+++ precache-manifest.0120de19f4ea7071ef6c3cc39179c080.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "bd52d37a99c9fcf3ca63",
+    "url": "/clicky-game/static/js/main.ed81866f.chunk.js"
+  },
+  {
+    "revision": "8b28b04b5b06edd2dc22",
+    "url": "/clicky-game/static/js/2.dc948b19.chunk.js"
+  },
+  {
+    "revision": "bd52d37a99c9fcf3ca63",
+    "url": "/clicky-game/static/css/main.c1e9fa73.chunk.css"
+  },
+  {
+    "revision": "8636dcb5316216d5cbc7b343307d27c5",
+    "url": "/clicky-game/index.html"
+  }
+];
